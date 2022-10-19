6e0afe8dd90abe731db9bc896a543af772493c91
rvlwldev <rvlwlasd77@gmail.com>
2022-10-19 21:40:47 +0900

    js 주석

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,15 +1,38 @@
 if (typeof jQuery == undefined) {
     alert("제이쿼리 안됨")
-    // 바닐라JS는 너무 불편해서 제이쿼리 쓰는게 좋을꺼같은데
 }
 
 // 메모 추가
 $('#b_add').click(function () {
     // 추가할 내용
-    let value = document.getElementById("add_input").value;
+    // let value = document.getElementById("add_input").value;
+    let value = $('#add_input').val();
+
+    // $.ajax({
+    //     type: "POST",
+    //     url: "/add",
+    //     dataType: 'json',
+    //     contentType: 'application/json',
+    //     data: value,
+    //     success: success(),
+    //     error: fail()
+    // });
 
     alert("추가할 내용 : " + value);
 });
+
+// 추가
+// 삭제
+// 수정
+
+
+function success(json) {
+
+}
+
+function fail() {
+
+}
 
 // 각 리스트 클릭 시
 $(".checkbox").click(function (e) {
@@ -18,9 +41,7 @@ $(".checkbox").click(function (e) {
     let isCheck = String(li.attr('class')) == "completed" ? true : false;
 
     // 백엔드 처리
-    // $ajax(
 
-    // )
 
 
     // 처리 이후 class 수정
