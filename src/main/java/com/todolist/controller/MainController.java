bc2f67a49d64304682549b06503e8a683c34c1aa
rvlwldev <rvlwlasd77@gmail.com>
2022-10-19 20:42:34 +0900

    main js css

--- src/main/java/com/todolist/controller/MainController.java
+++ src/main/java/com/todolist/controller/MainController.java
@@ -2,13 +2,17 @@ package com.todolist.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class MainController {
 
     @GetMapping("/main")
-    public String getMain() {
+    public ModelAndView getMain() {
 
-        return "main";
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("/main.html");
+
+        return mv;
     }
 }
